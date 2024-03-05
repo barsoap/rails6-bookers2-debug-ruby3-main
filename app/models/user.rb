cef17d7db962ca05a35a539afa92e2345763232c
EC2 Default User <ec2-user@ip-172-31-0-218.ap-northeast-1.compute.internal>
2024-03-05 17:29:56 +0000

    [Finish]assignment4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,16 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+
+  #自分がフォローする側の関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  #与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :following, through: :relationships, source: :followed
+  #自分がフォローされる側の関係
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  #被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -16,5 +26,20 @@ class User < ApplicationRecord
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  # フォローするとき
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  # フォローを外すとき
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  # フォローしているか確認するとき
+  def following?(user)
+    following.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followers'
+  get 'relationships/following'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users
@@ -10,7 +12,11 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy] #post_commentsコントローラのアクション
     resource :like, only: [:create, :destroy] #単数形にすると、/:idがURLに含まれなくなる。(いいね削除の際いいねのID必要ない為)
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get "followers" => "relationships#followers", as: "followers"
+    get "following" => "relationships#following", as: "following"
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
